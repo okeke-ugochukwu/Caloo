2c5e7b5e4e3e59b9b719e8ed4641c05fe95b45a3
mr.pianoman_ <success.US7@gmail.com>
2021-02-23 15:10:32 +0100

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,4 +1,7 @@
-//buttons change functions
+var buttons = $("button");
+console.log(buttons.length);
+
+//buttons-set change functions
 $("#special").click(function () {
     $("#grid-box-1").css("display", "none");
     $("#grid-box-2").css("display", "grid");
@@ -9,22 +12,71 @@ $("#special-2").click(function () {
     $("#grid-box-2").css("display", "none");
 })
 
-//input  functions
-$("input").click(function () {
-    var inputValue = this.value;
-    $("#active-calc").append(inputValue);
-    console.log(inputValue);
+//button functions
+$("button").click(function () {
+    var buttonLabel = this.innerHTML;
+    $("#active-calc").append(buttonLabel);
+
+    var buttonValue = this.value; //this is just for the display
+    $(".active_values").append(buttonValue)
+
+    var activeValues = $(".active_values").html(); //this are the actual values being used in calculation, hidden div in html
+    localStorage.setItem("activeValues", activeValues);
 
 })
 
-//clear and special functions
+//clear and special-div functions
 $("#reset").click(function () {
-    document.getElementById("active-calc").innerHTML = "";
+    document.getElementById("active-calc").innerHTML = "";         //clears display 
     document.getElementById("result").innerHTML = "";
-    inputValue = undefined;
+    $(".active_values").html("");                           //clears hidden div 
+    buttonValue = undefined;
 })
 $("#special").click(function () {
     document.getElementById("active-calc").innerHTML = "";
     document.getElementById("result").innerHTML = "";
-    inputValue = undefined;
+    buttonValue = undefined;
+})
+
+
+//calculation
+$("#equals-sign").click(function () {
+    var activeValues  = localStorage.getItem("activeValues")
+    console.log(activeValues);
+
+    var result = eval(activeValues);
+    localStorage.setItem("active_answer", result);
+   $("#result").html(result);
+})
+
+//Use previous answer
+$("#active_answer").click(function() {
+    var activeAnswer = localStorage.getItem("active_answer");
+    console.log(activeAnswer);
+
+    var display = document.getElementById("active-calc").innerHtml;
+    console.log(display);
+    if (display = " ") {
+        $("#active-calc").append("Ans");
+        $(".active_values").append  (activeAnswer);
+        var activeValues = $(".active_values").html();      //this are the actual values being used in calculation, hidden div in html
+        localStorage.setItem("activeValues", activeValues);
+    }
+    else {
+       alert("clear screen first")
+    }
+})
+
+// backspace function
+$("#backspace").click(function(){
+    var valuesOnDisplay = $("#active-calc").html();             // get text on display
+    var valuesActual =  $(".active_values").html();          // get actual values from hidden div
+
+    var newDisplayValues = valuesOnDisplay.toString().slice(0, -1);                   // remove last character from text on display and create new text
+    var newActualValues = valuesActual.toString().slice(0, -1);                 // remove last character from actual values       " "     " "     " "  
+
+    $("#active-calc").html(newDisplayValues);                        // input new text in display
+    $(".active_values").html(newActualValues);                  //input new values in hidden div
+
+    localStorage.setItem("activeValues", newActualValues);    //set new values up for re-use
 })
